7adb77d3b3d83631eca45f6f0ebf7f97c3af82f8
Ishita Jain <ishita9810@gmail.com>
2025-01-17 22:53:20 +0530

    setup input fields

--- loginform/src/App.js
+++ loginform/src/App.js
@@ -1,13 +1,17 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <h1>
-            login from
-            </h1>        
+          <div>
+            <input placeholder='first name'/>
+            <input placeholder='second name'/>
+            <input placeholder='Enter you semail'/>
+            <input placeholder='password'/>
+            <button>Submit</button>
+            </div>        
       </header>
     </div>
   );
